8cbe6b986bd55ef387435d0a0daaedbf386f0dd6
borisdubinin <borisdubinin01@gmail.com>
2026-03-17 21:44:40 +0300

    initial

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter out = response.getWriter();
+        int a = 5;
+        a = 5 + 5;
+        out.print("Привет");
+        out.close();
+    }
+}

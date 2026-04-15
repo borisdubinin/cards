705f8540326f69517cec0eb875e77bdd817e9672
borisdubinin <borisdubinin01@gmail.com>
2026-04-15 22:12:33 +0300

    logging

--- src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.exception;
 
 import com.example.dto.ErrorResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,23 +9,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponseDto handleEntityNotFound(EntityNotFoundException e) {
+        log.debug("Entity wasn't found: %s".formatted(e.getMessage()));
         return new ErrorResponseDto(e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponseDto handleIllegalArgument(IllegalArgumentException e) {
+        log.debug("Illegal argument encountered: %s".formatted(e.getMessage()));
         return new ErrorResponseDto(e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponseDto handleArgumentNotValid(MethodArgumentNotValidException e) {
+        log.debug("Illegal argument was received: %s".formatted(e.getMessage()));
         return new ErrorResponseDto(e.getMessage());
     }
 }

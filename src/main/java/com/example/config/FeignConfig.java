0d19baade1a50ec00286d7e8989802110fdfe41f
borisdubinin <borisdubinin01@gmail.com>
2026-05-03 13:02:29 +0300

    even simpler

--- src/main/java/com/example/config/FeignConfig.java
+++ src/main/java/com/example/config/FeignConfig.java
@@ -9,15 +9,14 @@ import feign.auth.BasicAuthRequestInterceptor;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import feign.slf4j.Slf4jLogger;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
 
-    @Bean(name = "accountsRequestInterceptor")
-    public RequestInterceptor accountsAuthRequestInterceptor(FeignProperties feignProperties) {
+    @Bean
+    public RequestInterceptor accountsRequestInterceptor(FeignProperties feignProperties) {
         return new BasicAuthRequestInterceptor(
                 feignProperties.getAccountsUsername(),
                 feignProperties.getAccountsPassword()
@@ -27,13 +26,13 @@ public class FeignConfig {
     @Bean
     public AccountClient accountClient(
             FeignProperties feignProperties,
-            @Qualifier(value = "accountsRequestInterceptor") RequestInterceptor requestInterceptor) {
+            RequestInterceptor accountsRequestInterceptor) {
         return Feign.builder()
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
                 .logger(new Slf4jLogger(AccountClient.class))
                 .logLevel(Logger.Level.FULL)
-                .requestInterceptor(requestInterceptor)
+                .requestInterceptor(accountsRequestInterceptor)
                 .target(AccountClient.class, feignProperties.getAccountsUrl());
     }
 }
